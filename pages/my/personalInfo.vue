c28107e872b001b9941cca693bf308e1264cb80c
genglw <760679216@qq.com>
2019-12-13 10:29:37 +0800

    git提交

--- pages/my/personalInfo.vue
+++ pages/my/personalInfo.vue
@@ -1,9 +1,59 @@
 <template>
-	<view>yonghu</view>
+	<view class="list">
+		<inputField type="picker" placeholder="自动获得焦点"></inputField>
+	</view>
 </template>
 
 <script>
+	import {
+		mapState,
+		mapActions,
+		mapGetters,
+		mapMutations
+	} from 'vuex'
+	import {
+		uniPopup,
+		uniList,
+		uniListItem,
+	} from "@dcloudio/uni-ui"
+	import inputField from "../../components/form/inputField"
+
+	export default {
+
+		onLoad() {
+
+		},
+
+		computed: {
+			...mapState('home', [
+				'individualPoints',
+			]),
+
+		},
+
+		components: {
+			uniPopup,
+			uniList,
+			uniListItem,
+			inputField,
+		},
+
+		methods: {
+			...mapActions('authed', [
+				'getWxAppNameAction',
+			]),
+
+		}
+	}
 </script>
 
-<style>
+<style lang="less">
+	.avatar {
+		width: 70rpx;
+		height: 70rpx;
+		float: right;
+		border-radius: 50%;
+		// overflow:hidden;
+		display:inline-block;
+	}
 </style>
